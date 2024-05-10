cc7791368d1e905f79f664ac8fd6a74a7eafe57f
hochenchong <hochenchong@gmail.com>
2024-05-10 22:20:21 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <router-link to="/">Home</router-link>
+  <span class="link-separator"></span>
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
+</template>
+
+<style>
+.link-separator {
+  margin: 0 8px; /* 设置间隔大小，可以根据需要进行调整 */
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<!-- Home.vue -->
+<template>
+    <div class="home">
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
+  <style scoped>
+  /* styles for Home.vue */
+  </style>
+  
